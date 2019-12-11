855a9f4d270ee9a4ba628267701c57bd523fe557
sedanerdem <sedanerdem@hotmail.com>
2019-12-11 23:34:34 +0300

    Task 3: email replacer methods is completed.

--- wizkidsDemo/Program.cs
+++ wizkidsDemo/Program.cs
@@ -14,14 +14,22 @@ namespace wizkidsDemo
             Console.WriteLine("Task1: Enter palindrome string : ");
             bool isPalindrome = palindrome(Console.ReadLine());
             Console.WriteLine(isPalindrome);
+            Console.WriteLine("Press a button task 2");
             Console.ReadKey();
             Console.WriteLine("----------------------------------");
             Console.WriteLine("Task2: FooBar");
             fooBar();
+            Console.WriteLine("Press a button task 2");
             Console.ReadKey();
             Console.WriteLine("----------------------------------");
-
+            Console.WriteLine("Task3: Email Replacer");
+            Console.WriteLine("Replacer : ");
+            string replacer = Console.ReadLine();
+            string text = emailReplacer("Christian has the email address christian+123@gmail.com.Christian's friend, John Cave-Brown, has the email address john.cave-brown@gmail.com.John's daughter Kira studies at Oxford University and has the email adress Kira123@oxford.co.uk.Her Twitter handle is @kira.cavebrown.",replacer);
+            Console.WriteLine(text);
+            Console.WriteLine("Press a button task 2");
             Console.ReadKey();
+            Console.WriteLine("----------------------------------");
         }
 
 
@@ -60,6 +68,28 @@ namespace wizkidsDemo
                     Console.WriteLine(i);
                 }
             }
+        }
+        public static string emailReplacer(string text, string replace)
+        {
+            string[] words = text.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Contains("@"))
+                {
+                    string[] emailCandidate = words[i].Split('@');
+                    if (emailCandidate.Length == 2)
+                    {
+                        if (emailCandidate[0].Length > 0)
+                        {
+                            if (emailCandidate[1].Contains(".co"))
+                            {
+                                text = text.Replace(words[i], replace);
+                            }
+                        }
+                    }
+                }
+            }
+            return text;
         }
     }
 }

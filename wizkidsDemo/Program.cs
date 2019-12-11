535752df240149a0b488edeb0e2309959cf4fa05
sedanerdem <sedanerdem@hotmail.com>
2019-12-11 23:13:22 +0300

    Task 2: FooBar methods is completed.

--- wizkidsDemo/Program.cs
+++ wizkidsDemo/Program.cs
@@ -10,6 +10,56 @@ namespace wizkidsDemo
     {
         static void Main(string[] args)
         {
+            //Task 1 : palindrome strings
+            Console.WriteLine("Task1: Enter palindrome string : ");
+            bool isPalindrome = palindrome(Console.ReadLine());
+            Console.WriteLine(isPalindrome);
+            Console.ReadKey();
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Task2: FooBar");
+            fooBar();
+            Console.ReadKey();
+            Console.WriteLine("----------------------------------");
+
+            Console.ReadKey();
+        }
+
+
+        public static bool palindrome(string s)
+        {
+            bool isPalindrome = true;
+            int checkCount = (s.Length % 2 == 0) ? s.Length / 2 : (s.Length - 1)/2;
+            for (int i = 0, j = s.Length-1; i < checkCount; i++, j--)
+            {
+                if (s[i] != s[j])
+                {
+                    isPalindrome = false;
+                }
+            }
+            return isPalindrome;
+        }
+
+        public static void fooBar()
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                if (i%15==0)
+                {
+                    Console.WriteLine("FooBar");
+                }
+                else if (i%5==0)
+                {
+                    Console.WriteLine("Bar");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Foo");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
